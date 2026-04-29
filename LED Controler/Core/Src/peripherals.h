385473575e0b4c22a633ded77491b98ecace38f1
pedrobegali1 <pedrobegali@alunos.utfpr.edu.br>
2026-04-29 13:59:38 -0300

    Add files via upload

---
+++ LED Controler/Core/Src/peripherals.h
@@ -0,0 +1,13 @@
+/*
+ * peripherals.h
+ *
+ *  Created on: Apr 7, 2026
+ *      Author: Pedro
+ */
+
+#ifndef SRC_PERIPHERALS_H_
+#define SRC_PERIPHERALS_H_
+
+void init_GPIO_SETUP(void);
+
+#endif /* SRC_PERIPHERALS_H_ */
